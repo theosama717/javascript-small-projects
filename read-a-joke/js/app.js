7f24440bf5fbf287057b89b40d1fbe66b8b08fdb
Muhammad Osama <104783708+theosama717@users.noreply.github.com>
2024-06-28 12:14:57 +0500

    Update app.js

--- read-a-joke/js/app.js
+++ read-a-joke/js/app.js
@@ -11,7 +11,30 @@ btnEl.addEventListener('click', async (event) => {
 
     const response = await fetch('https://api.api-ninjas.com/v1/jokes?limit=1', {
         method: 'GET', 
+        headers: {'X-Api-Key': 'const jokeEl = document.querySelector('#joke'); 
+const btnEl = document.querySelector('#button'); 
+
+
+btnEl.addEventListener('click', async (event) => {
+
+    jokeEl.textContent = 'updating...'; 
+
+    btnEl.disabled = true;
+    btnEl.textContent = 'Loading...';
+
+    const response = await fetch('https://api.api-ninjas.com/v1/jokes?limit=1', {
+        method: 'GET', 
         headers: {'X-Api-Key': 'AIej0Id4XRo6n9GkEV7b/w==NaZk4CRFqhAurdbb'}, 
+        contentType: 'application/json'
+    });
+
+    const jsonResponse = await response.json(); 
+
+    btnEl.disabled = false; 
+    btnEl.textContent = 'Tell me a joke'; 
+
+    jokeEl.textContent = jsonResponse[0]['joke'];
+})'}, 
         contentType: 'application/json'
     });
 
